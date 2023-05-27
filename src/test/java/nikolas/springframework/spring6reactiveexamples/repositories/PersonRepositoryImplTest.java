4845964ce0555d346f97be9e29b1d5b4bbd69553
sznikolas <szocs.nikolas@gmail.com>
2023-05-27 10:50:57 +0200

    Mono operations

---
+++ src/test/java/nikolas/springframework/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
@@ -0,0 +1,43 @@
+package nikolas.springframework.spring6reactiveexamples.repositories;
+
+import nikolas.springframework.spring6reactiveexamples.domain.Person;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonRepositoryImplTest {
+
+    PersonRepository personRepository = new PersonRepositoryImpl();
+
+    //this is not preferred
+    @Test
+    void testMonoByIdBlock() {
+        Mono<Person> personMono = personRepository.getById(1);
+
+        Person person = personMono.block();
+
+        System.out.println(person.toString());
+    }
+
+    //more preferred method than blocking
+    @Test
+    void testGetByIdSubscriber() {
+        Mono<Person> personMono = personRepository.getById(1);
+
+        personMono.subscribe(person -> {
+            System.out.println(person.toString());
+        });
+    }
+
+    @Test
+    void testMapOperation() {
+        Mono<Person> personMono = personRepository.getById(1);
+
+        personMono.map(person -> {
+            return person.getFirstName();
+        }).subscribe(firstName -> {
+            System.out.println(firstName);
+        });
+    }
+}
